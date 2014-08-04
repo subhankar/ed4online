74c6c176f96aafca6a6f3af14ff30024a1b1c16f
Subhankar <subhankar.das@us.dynpro.com>
2014-08-04 13:50:03 +0530

    Modifying

--- view.detail.php
+++ view.detail.php
@@ -3,14 +3,10 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 require_once('include/MVC/View/views/view.detail.php');
 class ContactsViewDetail extends ViewDetail{
 	 function display() {
-		echo 'XXXX';		
-		
 		if($this->bean->contact_type_c == 'Student'){
-			echo $this->bean->contact_type_c;
 			unset($this->dv->defs['panels']['LBL_PANEL_ADVANCED']);
 		}
 		if($this->bean->contact_type_c == 'Business'){
-			echo $this->bean->contact_type_c;
 			unset($this->dv->defs['panels']['LBL_PANEL_ASSIGNMENT']);
 		}
 		//Clear Template Cache
